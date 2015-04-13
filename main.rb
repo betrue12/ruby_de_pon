8806ca093034c24c50a5ff43761b98d6ab78be6e
betrue12 <betrue12@users.noreply.github.com>
2015-04-13 22:36:00 +0900

    rubocop

--- main.rb
+++ main.rb
@@ -23,10 +23,10 @@ VANISH_SPEED = 3.0
 COM_INPUT_CYCLE = 3
 
 # buttons of game pad
-BUTTONS = [ P_BUTTON0, P_BUTTON1, P_BUTTON2, P_BUTTON3,
-            P_BUTTON4, P_BUTTON5, P_BUTTON6, P_BUTTON7,
-            P_BUTTON8, P_BUTTON9, P_BUTTON10, P_BUTTON11,
-            P_BUTTON12, P_BUTTON13, P_BUTTON14, P_BUTTON15 ]
+BUTTONS = [P_BUTTON0, P_BUTTON1, P_BUTTON2, P_BUTTON3,
+           P_BUTTON4, P_BUTTON5, P_BUTTON6, P_BUTTON7,
+           P_BUTTON8, P_BUTTON9, P_BUTTON10, P_BUTTON11,
+           P_BUTTON12, P_BUTTON13, P_BUTTON14, P_BUTTON15]
 
 class Panel
   attr_accessor :x,
@@ -56,7 +56,7 @@ class Panel
   def draw(offset_slide)
     axis_x = @x * PANEL_SIZE
     axis_y = WINDOW_Y - y * PANEL_SIZE + @offset_y.floor - offset_slide.floor
-    Window.draw_alpha(axis_x , axis_y, @image, @alpha.floor)
+    Window.draw_alpha(axis_x, axis_y, @image, @alpha.floor)
   end
 
   def vanishable?
@@ -105,15 +105,13 @@ class Panel
   def fixed?
     @is_fixed
   end
-
 end
-
 
 class Cursor
   attr_accessor :x,
                 :y
 
-  def initialize(x,y)
+  def initialize(x, y)
     @x = x
     @y = y
     @image = Image.load('./resources/cursor/cursor.png') # width: PANEL_SIZE * 2, height: PANEL_SIZE
@@ -129,7 +127,7 @@ class Cursor
   def draw(offset_slide)
     axis_x = @x * PANEL_SIZE
     axis_y = WINDOW_Y - y * PANEL_SIZE - offset_slide.floor
-    Window.draw(axis_x , axis_y, @image)
+    Window.draw(axis_x, axis_y, @image)
   end
 end
 
@@ -147,36 +145,33 @@ class Score
 
   def combo_bonus(combo)
     @score += (combo > 13) ? 0 : 150 * combo - 250
-    @messages.push(combo.to_s + " combo!")
+    @messages.push(combo.to_s + ' combo!')
   end
 
   def many_bonus(num)
     if num < 31
-      @score += ((2 * num**2)/10.0).floor * 10
+      @score += ((2 * num**2) / 10.0).floor * 10
     elsif num == 31
       @score += 0
     else
       @score += 33000
     end
-    @messages.push(num.to_s + " vanish!")
+    @messages.push(num.to_s + ' vanish!')
   end
 
   def age
     @del_message_count = (@del_message_count + 1) % (60 * 5)
-    if @del_message_count == 0
-      @messages.delete_at(0) if @messages
-    end
+    @messages.delete_at(0) if @messages if @del_message_count == 0
   end
 
   def draw
-    str = "Score: " + @score.to_s + "\n\n"
-    @messages.each{|message|
+    str = 'Score: ' + @score.to_s + "\n\n"
+    @messages.each do |message|
       str += message + "\n"
-    }
+    end
     Window.draw_font(FIELD_X + 40, 80, str, @font)
   end
 end
-
 
 class Field
   attr_accessor :panels,
@@ -186,43 +181,43 @@ class Field
   def initialize
     is_lined_without_vanish = false
     until is_lined_without_vanish
-      @panels = Array.new(PANEL_X){ Array.new(PANEL_Y) }
+      @panels = Array.new(PANEL_X) { Array.new(PANEL_Y) }
       is_lined_without_vanish = true # tmp
-      (0...(PANEL_Y/2)).each {|y|
+      (0...(PANEL_Y / 2)).each do |y|
         make_newline(y)
-      }
+      end
 
       # check only vertical vanishing,
       # because make_newline checks horizontal vanishing
-      (0...PANEL_X).each {|x|
-        (1...(PANEL_Y - 2)).each {|y|
+      (0...PANEL_X).each do |x|
+        (1...(PANEL_Y - 2)).each do |y|
           panel = @panels[x][y]
-          above_panel1 = @panels[x][y+1]
-          above_panel2 = @panels[x][y+2]
+          above_panel1 = @panels[x][y + 1]
+          above_panel2 = @panels[x][y + 2]
           if panel && panel.vanish_with?(above_panel1, above_panel2)
             is_lined_without_vanish = false
           end
-        }
-      }
+        end
+      end
     end
 
     @offset_slide = 0.0
     @is_continue = true
-    @cursor = Cursor.new((PANEL_X - 1)/2, PANEL_Y/2)
+    @cursor = Cursor.new((PANEL_X - 1) / 2, PANEL_Y / 2)
     @is_force_sliding = false
     @score = Score.new
   end
 
   def make_newline(y)
-    (0...PANEL_X).each {|x|
-      panel = Panel.new(x, y, rand(0...COLORS) )
-      left_panel1 = @panels[x-1][y]
-      left_panel2 = @panels[x-2][y]
+    (0...PANEL_X).each do |x|
+      panel = Panel.new(x, y, rand(0...COLORS))
+      left_panel1 = @panels[x - 1][y]
+      left_panel2 = @panels[x - 2][y]
       while x >= 2 && panel.color == left_panel1.color && panel.color == left_panel2.color
-        panel = Panel.new(x, y, rand(0...COLORS) )
+        panel = Panel.new(x, y, rand(0...COLORS))
       end
       @panels[x][y] = panel
-    }
+    end
   end
 
   def handle_force_slide
@@ -233,18 +228,17 @@ class Field
     x = @cursor.x
     y = @cursor.y
     panel_l = @panels[x][y]
-    panel_r = @panels[x+1][y]
-    above_panel_l = @panels[x][y+1]
-    above_panel_r = @panels[x+1][y+1]
-    below_panel_l = @panels[x][y-1]
-    below_panel_r = @panels[x+1][y-1]
+    panel_r = @panels[x + 1][y]
+    above_panel_l = @panels[x][y + 1]
+    above_panel_r = @panels[x + 1][y + 1]
+    below_panel_l = @panels[x][y - 1]
+    below_panel_r = @panels[x + 1][y - 1]
 
     # condition of exchange impossible
     return if panel_l && !panel_l.vanishable?
     return if panel_r && !panel_r.vanishable?
     return if above_panel_l && !above_panel_l.fixed?
     return if above_panel_r && !above_panel_r.fixed?
-
 
     if panel_l
       panel_l.x += 1 # to right
@@ -255,38 +249,37 @@ class Field
       panel_r.unfix unless below_panel_l && below_panel_l.fixed?
     end
 
-
     @panels[x][y] = panel_r
-    @panels[x+1][y] = panel_l
+    @panels[x + 1][y] = panel_l
   end
 
   def slide
-    (0...PANEL_X).each {|x|
-      (0...PANEL_Y).each {|y|
+    (0...PANEL_X).each do |x|
+      (0...PANEL_Y).each do |y|
         panel = @panels[x][y]
         return if panel && (panel.vanishing? || !panel.fixed?)
-      }
-    }
+      end
+    end
 
     @offset_slide += (@is_force_sliding ? 3.0 : 1.0) * SLIDE_SPEED
 
     if @offset_slide >= PANEL_SIZE
       @offset_slide -= PANEL_SIZE
-      (0...PANEL_X).each {|x|
-        PANEL_Y.downto(0) {|y| # need to exec from top
+      (0...PANEL_X).each do |x|
+        PANEL_Y.downto(0) do |y| # need to exec from top
           panel = @panels[x][y]
           next unless panel
           @panels[x][y] = nil
           panel.y += 1
-          @panels[x][y+1] = panel
+          @panels[x][y + 1] = panel
           panel.lighten
           @is_force_sliding = false
           if panel.y == PANEL_Y - 1
             die
             break
           end
-        }
-      }
+        end
+      end
       @cursor.y += 1
       make_newline(0)
     end
@@ -296,45 +289,45 @@ class Field
     vanish_num_for_score = 0
     combo_for_score = 1
 
-    (0...PANEL_X).each {|x|
-      (1...PANEL_Y).each {|y|
+    (0...PANEL_X).each do |x|
+      (1...PANEL_Y).each do |y|
         base = @panels[x][y]
         next unless base && base.vanishable?
 
         # horizontal
         if x + 2 < PANEL_X
-          right_panel1 = @panels[x+1][y]
-          right_panel2 = @panels[x+2][y]
+          right_panel1 = @panels[x + 1][y]
+          right_panel2 = @panels[x + 2][y]
           if base.vanish_with?(right_panel1, right_panel2)
             vanishing_panels = [base, right_panel1, right_panel2]
-            combo = vanishing_panels.map{|panel| panel.combo }.max
-            vanishing_panels.each{|panel|
+            combo = vanishing_panels.map { |panel| panel.combo }.max
+            vanishing_panels.each do |panel|
               panel.combo = combo
               panel.to_vanish
-            }
+            end
             combo_for_score = [combo_for_score, combo].max
           end
         end
 
         # vertical
         if y + 2 < PANEL_Y
-          above_panel1 = @panels[x][y+1]
-          above_panel2 = @panels[x][y+2]
+          above_panel1 = @panels[x][y + 1]
+          above_panel2 = @panels[x][y + 2]
           if base.vanish_with?(above_panel1, above_panel2)
             vanishing_panels = [base, above_panel1, above_panel2]
-            combo = vanishing_panels.map{|panel| panel.combo }.max
-            vanishing_panels.each{|panel|
+            combo = vanishing_panels.map { |panel| panel.combo }.max
+            vanishing_panels.each do |panel|
               panel.combo = combo
               panel.to_vanish
-            }
+            end
             combo_for_score = [combo_for_score, combo].max
           end
         end
-      }
-    }
+      end
+    end
 
-    (0...PANEL_X).each {|x|
-      (1...PANEL_Y).each {|y|
+    (0...PANEL_X).each do |x|
+      (1...PANEL_Y).each do |y|
         panel = @panels[x][y]
         next unless panel
         if panel.to_vanish?
@@ -352,18 +345,17 @@ class Field
           end
           @panels[x][y] = nil
         end
-      }
-    }
+      end
+    end
     @score.vanish_score(vanish_num_for_score)
     @score.combo_bonus(combo_for_score) if combo_for_score > 1
     @score.many_bonus(vanish_num_for_score) if vanish_num_for_score > 3
     @score.age
-
   end
 
   def fall_panels
-    (0...PANEL_X).each {|x|
-      (1...PANEL_Y).each {|y|    # need to exec from bottom
+    (0...PANEL_X).each do |x|
+      (1...PANEL_Y).each do |y|    # need to exec from bottom
         panel = @panels[x][y]
         next if !panel || panel.vanishing?
 
@@ -387,29 +379,29 @@ class Field
           below_panel = @panels[x][panel.y - 1]
           panel.fix if below_panel && below_panel.fixed?
         end
-      }
-    }
+      end
+    end
   end
 
   def calc_columns_height
     columns_height = Array.new(PANEL_X)
-    (0...PANEL_X).each {|x|
-      PANEL_Y.downto(0){|y|
+    (0...PANEL_X).each do |x|
+      PANEL_Y.downto(0) do |y|
         if @panels[x][y]
           columns_height[x] = y
           break
         end
-      }
-    }
+      end
+    end
     return columns_height
   end
 
   def draw_elements
-    (0...PANEL_X).each {|x|
-      (0...PANEL_Y).each {|y|
+    (0...PANEL_X).each do |x|
+      (0...PANEL_Y).each do |y|
         @panels[x][y].draw(@offset_slide) if @panels[x][y]
-      }
-    }
+      end
+    end
     @cursor.draw(@offset_slide)
     @score.draw
   end
@@ -427,7 +419,6 @@ class Field
   end
 end
 
-
 class Brain
   def initialize
     @input_queue = []
@@ -439,7 +430,7 @@ class Brain
     @cycle_count = (@cycle_count + 1) % COM_INPUT_CYCLE
     return nil unless @cycle_count == 0
 
-    @input_queue = self.make_inputs(field) if @input_queue.length == 0
+    @input_queue = make_inputs(field) if @input_queue.length == 0
     return @input_queue.shift
   end
 
@@ -447,7 +438,7 @@ class Brain
     return [nil] if field.force_sliding?
 
     columns_height = field.calc_columns_height
-    return [{:force_slide => true}] if columns_height.max < PANEL_Y - 4
+    return [{ :force_slide => true }] if columns_height.max < PANEL_Y - 4
 
     targets = balance(columns_height) ||
               try_vanish(field) ||
@@ -457,50 +448,50 @@ class Brain
   end
 
   def balance(columns_height)
-    (0...(PANEL_X - 1)).each {|x|
+    (0...(PANEL_X - 1)).each do |x|
       two_height = columns_height[x, 2]
       if (two_height[0] - two_height[1]).abs >= 2
-        return {:x => x, :y => two_height.max}
+        return { :x => x, :y => two_height.max }
       end
-    }
+    end
     return nil
   end
 
   def try_vanish(field)
     panels = field.panels
-    row_existing_color = Array.new(PANEL_Y){ [] }
+    row_existing_color = Array.new(PANEL_Y) { [] }
 
-    (1...(PANEL_Y - 2)).each {|y|
-      row_existing_color[y] = (0...PANEL_X).to_a.map{|x| panels[x][y] && panels[x][y].color}.uniq.select{|elem| elem}
+    (1...(PANEL_Y - 2)).each do |y|
+      row_existing_color[y] = (0...PANEL_X).to_a.map { |x| panels[x][y] && panels[x][y].color }.uniq.select { |elem| elem }
       next if y <= 2
-      three_rows_color = row_existing_color[y] + row_existing_color[y-1] + row_existing_color[y-2]
-      (0...COLORS).each {|color|
+      three_rows_color = row_existing_color[y] + row_existing_color[y - 1] + row_existing_color[y - 2]
+      (0...COLORS).each do |color|
         if three_rows_color.count(color) == 3
           target_rows = [y - 2, y - 1, y]
-          now_x = target_rows.map{|tmp_y|
-            (0...PANEL_X).each {|x|
+          now_x = target_rows.map do |tmp_y|
+            (0...PANEL_X).each do |x|
               break x if panels[x][tmp_y] && panels[x][tmp_y].color == color
-            }
-          }
+            end
+          end
           base_x = now_x[2]
           targets = []
-          (now_x.length - 1).times {|i|
+          (now_x.length - 1).times do |i|
             x = now_x[i]
             tmp_y = target_rows[i]
             if x >= base_x
-              (x - 1).downto(base_x){|tmp_x|
-                targets.push({:x => tmp_x, :y => tmp_y})
-              }
+              (x - 1).downto(base_x) do |tmp_x|
+                targets.push(:x => tmp_x, :y => tmp_y)
+              end
             else
-              x.upto(base_x - 1){|tmp_x|
-                targets.push({:x => tmp_x, :y => tmp_y})
-              }
+              x.upto(base_x - 1) do |tmp_x|
+                targets.push(:x => tmp_x, :y => tmp_y)
+              end
             end
-          }
+          end
           return targets
         end
-      }
-    }
+      end
+    end
     return nil
   end
 
@@ -508,34 +499,33 @@ class Brain
     x = rand(0...(PANEL_X - 1))
     max_y = columns_height[x, 2].max
     y = rand(0..max_y)
-    return {:x => x, :y => y}
+    return { :x => x, :y => y }
   end
 
   def inputs_to_move_and_exchange(now_cursor, targets)
     inputs = []
-    prev = {:x => now_cursor.x, :y =>now_cursor.y}
+    prev = { :x => now_cursor.x, :y => now_cursor.y }
     targets = [targets] if targets.is_a?(Hash)
 
-    targets.each{|target|
+    targets.each do |target|
       x_dist = target[:x] - prev[:x]
       y_dist = target[:y] - prev[:y]
       if x_dist > 0
-        inputs += [{:right => true}] * x_dist
+        inputs += [{ :right => true }] * x_dist
       else
-        inputs += [{:left => true}] * (-x_dist)
+        inputs += [{ :left => true }] * (-x_dist)
       end
       if y_dist > 0
-        inputs += [{:up => true}] * y_dist
+        inputs += [{ :up => true }] * y_dist
       else
-        inputs += [{:down => true}] * (-y_dist)
+        inputs += [{ :down => true }] * (-y_dist)
       end
-      inputs += [{:exchange => true}]
+      inputs += [{ :exchange => true }]
       prev = target
-    }
+    end
     return inputs
   end
 end
-
 
 module Mode
   module_function # use all funtions in Window.loop
@@ -552,15 +542,15 @@ module Mode
               "\n" +
               "*Game pad*\n" +
               "Push any button of pad\n" +
-              "for button config, and then start"
+              'for button config, and then start'
     Window.draw_font(10, 100, message, font)
 
     return 'main' if Input.key_push?(K_SPACE)
     return 'demo' if Input.key_push?(K_Z)
 
-    BUTTONS.each{|button|
+    BUTTONS.each do |button|
       return 'pad_config' if Input.pad_push?(button)
-    }
+    end
 
     return 'select'
   end
@@ -569,32 +559,32 @@ module Mode
     font = Font.new(16)
     case step
     when 0
-      Window.draw_font(10, 100, "Push button for exchange panels", font)
-      BUTTONS.each{|button|
+      Window.draw_font(10, 100, 'Push button for exchange panels', font)
+      BUTTONS.each do |button|
         if Input.pad_push?(button)
           Input.set_config(button, K_SPACE)
           return step + 1
         end
-      }
+      end
     when 1
-      Window.draw_font(10, 100, "Push button for slide up fast", font)
-      BUTTONS.each{|button|
+      Window.draw_font(10, 100, 'Push button for slide up fast', font)
+      BUTTONS.each do |button|
         if Input.pad_push?(button)
           Input.set_config(button, K_Z)
           return step + 1
         end
-      }
+      end
     end
     return step
   end
 
   def main(field)
-    input_hash = {:up          => Input.key_push?(K_UP),
-                  :down        => Input.key_push?(K_DOWN),
-                  :right       => Input.key_push?(K_RIGHT),
-                  :left        => Input.key_push?(K_LEFT),
-                  :exchange    => Input.key_push?(K_SPACE),
-                  :force_slide => Input.key_down?(K_Z)      }
+    input_hash = { :up          => Input.key_push?(K_UP),
+                   :down        => Input.key_push?(K_DOWN),
+                   :right       => Input.key_push?(K_RIGHT),
+                   :left        => Input.key_push?(K_LEFT),
+                   :exchange    => Input.key_push?(K_SPACE),
+                   :force_slide => Input.key_down?(K_Z)      }
     next_mode = play(field, input_hash)
     return next_mode
   end
@@ -622,7 +612,7 @@ module Mode
     message = "*Game Over*\n" +
               "\n" +
               "push SPACE or exchange button\n" +
-              "for start game again"
+              'for start game again'
     font = Font.new(16)
     Window.draw_font(10, 100, message, font)
     return Input.key_push?(K_SPACE) ? 'main' : 'game_over'
